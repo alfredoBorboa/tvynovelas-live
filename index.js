7626585821f19e6033b4a008ba8e10f7d703abe5
Alfredo Borboa Osorio <cactuses@prodigy.net.mx>
2015-02-12 19:09:51 -0600

    commit3

--- index.js
+++ index.js
@@ -12,9 +12,35 @@ app.use(express.static(__dirname + '/public' ));
 io.on('connection', function(socket){
   console.log('usuario conectado');
   socket.on('disconnect', function(){
-    console.log('usuario a la verga');
+    console.log('usuario desconectado');
   });
-});
+
+  socket.on('aCandidato1Jurado1', function(msg){
+    io.emit('voto', msg);
+    console.log("voto");
+  });
+
+  socket.on('aCandidato2Jurado1', function(msg){
+    io.emit('voto', msg);
+    console.log("voto");
+  });
+
+  socket.on('aCandidato3Jurado1', function(msg){
+    io.emit('voto', msg);
+    console.log("voto");
+  });
+
+  socket.on('aCandidato4Jurado1', function(msg){
+    io.emit('voto', msg);
+    console.log("voto");
+  });
+
+  socket.on('aCandidato5Jurado1', function(msg){
+    io.emit('voto', msg);
+    console.log("voto");
+  });
+
+});//termina io.on
 
 
 http.listen(3000, function(){
