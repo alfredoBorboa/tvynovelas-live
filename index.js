08762af118f8f4a38c9ae1d68e16b08330b7c3cd
Alfredo Borboa Osorio <cactuses@prodigy.net.mx>
2015-02-13 16:59:47 -0600

    Commit 2 13 de febrero

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-/*app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});*/
-
 app.use(express.static(__dirname + '/public' ));
 
 io.on('connection', function(socket){
