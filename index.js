62062066b86b360d7debfabf5950f43e1434fd3a
Alfredo Borboa Osorio <alfredo@MacBook-Pro-de-Alfredo.local>
2015-02-12 21:20:39 -0600

    Commit 4

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ app.use(express.static(__dirname + '/public' ));
 
 io.on('connection', function(socket){
   console.log('usuario conectado');
+ 
   socket.on('disconnect', function(){
     console.log('usuario desconectado');
   });
 
+  // categoria A jurado 1
   socket.on('aCandidato1Jurado1', function(msg){
     io.emit('voto', msg);
     console.log("voto");
@@ -36,6 +38,32 @@ io.on('connection', function(socket){
   });
 
   socket.on('aCandidato5Jurado1', function(msg){
+    io.emit('voto', msg);
+    console.log("voto");
+  });
+
+  // categoria A jurado 2
+  socket.on('aCandidato1Jurado2', function(msg){
+    io.emit('voto', msg);
+    console.log("voto");
+  });
+
+  socket.on('aCandidato2Jurado2', function(msg){
+    io.emit('voto', msg);
+    console.log("voto");
+  });
+
+  socket.on('aCandidato3Jurado2', function(msg){
+    io.emit('voto', msg);
+    console.log("voto");
+  });
+
+  socket.on('aCandidato4Jurado2', function(msg){
+    io.emit('voto', msg);
+    console.log("voto");
+  });
+
+  socket.on('aCandidato5Jurado2', function(msg){
     io.emit('voto', msg);
     console.log("voto");
   });
