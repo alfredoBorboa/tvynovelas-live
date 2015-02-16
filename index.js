053633b132d63c3a50b6b0352d0783d1e26c806d
Alfredo Borboa Osorio <alfredo@MacBook-Pro-de-Alfredo.local>
2015-02-16 06:18:53 -0600

    Commit 16 feb

--- index.js
+++ index.js
@@ -12,56 +12,13 @@ io.on('connection', function(socket){
     console.log('usuario desconectado');
   });
 
-  // categoria A jurado 1
-  socket.on('aCandidato1Jurado1', function(msg){
-    io.emit('voto', msg);
-    console.log("voto");
-  });
 
-  socket.on('aCandidato2Jurado1', function(msg){
-    io.emit('voto', msg);
-    console.log("voto");
+  socket.on( "votoDesdeJurado", function( msg ){
+      io.emit( "votoHaciaMonitor", msg );
   });
-
-  socket.on('aCandidato3Jurado1', function(msg){
-    io.emit('voto', msg);
-    console.log("voto");
-  });
-
-  socket.on('aCandidato4Jurado1', function(msg){
-    io.emit('voto', msg);
-    console.log("voto");
-  });
-
-  socket.on('aCandidato5Jurado1', function(msg){
-    io.emit('voto', msg);
-    console.log("voto");
-  });
-
-  // categoria A jurado 2
-  socket.on('aCandidato1Jurado2', function(msg){
-    io.emit('voto', msg);
-    console.log("voto");
-  });
-
-  socket.on('aCandidato2Jurado2', function(msg){
-    io.emit('voto', msg);
-    console.log("voto");
-  });
-
-  socket.on('aCandidato3Jurado2', function(msg){
-    io.emit('voto', msg);
-    console.log("voto");
-  });
-
-  socket.on('aCandidato4Jurado2', function(msg){
-    io.emit('voto', msg);
-    console.log("voto");
-  });
-
-  socket.on('aCandidato5Jurado2', function(msg){
-    io.emit('voto', msg);
-    console.log("voto");
+    
+  socket.on( "convocaSegundaVotacion", function( msg ){
+      io.emit( "ordenaSegundaVotacion", msg );
   });
 
 });//termina io.on
