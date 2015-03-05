f9d0e1ef51d6c01318289c063d76cb09fe2f7377
Alfredo Borboa Osorio <cactuses@prodigy.net.mx>
2015-03-04 20:15:53 -0600

    commit 4 de marzo

--- index.js
+++ index.js
@@ -29,13 +29,102 @@ io.on('connection', function(socket){
       io.emit( "desactivaOverlayLogoVotante", msg );
   });
     
-  socket.on( "siguienteSeccionMonitor", function( msg ){
+  /*socket.on( "siguienteSeccionMonitor", function( msg ){
       io.emit( "siguienteSeccionVotante", msg );
-  });
+  });*/
+  //--------------------cues para secciones en teclas  
+    socket.on( "veaA", function( msg ){
+        io.emit( "jveaA", msg );
+    });
+    
+    socket.on( "veaB", function( msg ){
+        io.emit( "jveaB", msg );
+    });
+    
+    socket.on( "veaC", function( msg ){
+        io.emit( "jveaC", msg );
+    });
+    
+    socket.on( "veaD", function( msg ){
+        io.emit( "jveaD", msg );
+    });
+    
+    socket.on( "veaE", function( msg ){
+        io.emit( "jveaE", msg );
+    });
+    
+    socket.on( "veaF", function( msg ){
+        io.emit( "jveaF", msg );
+    });
+    
+    socket.on( "veaG", function( msg ){
+        io.emit( "jveaG", msg );
+    });
+    
+    socket.on( "veaH", function( msg ){
+        io.emit( "jveaH", msg );
+    });
+    
+    socket.on( "veaI", function( msg ){
+        io.emit( "jveaI", msg );
+    });
+    
+    socket.on( "veaK", function( msg ){
+        io.emit( "jveaK", msg );
+    });
+    
+    socket.on( "veaL", function( msg ){
+        io.emit( "jveaL", msg );
+    });
+    
+    socket.on( "veaM", function( msg ){
+        io.emit( "jveaM", msg );
+    });
+    
+    socket.on( "veaN", function( msg ){
+        io.emit( "jveaN", msg );
+    });
+    
+    socket.on( "veaO", function( msg ){
+        io.emit( "jveaO", msg );
+    });
+    
+    socket.on( "veaP", function( msg ){
+        io.emit( "jveaP", msg );
+    });
+    
+    socket.on( "veaQ", function( msg ){
+        io.emit( "jveaQ", msg );
+    });
+    
+    socket.on( "veaR", function( msg ){
+        io.emit( "jveaR", msg );
+    });
+    
+    socket.on( "veaS", function( msg ){
+        io.emit( "jveaS", msg );
+    });
+    
+    socket.on( "veaT", function( msg ){
+        io.emit( "jveaT", msg );
+    });
+    
+    socket.on( "veaU", function( msg ){
+        io.emit( "jveaU", msg );
+    });
+    
+    socket.on( "veaV", function( msg ){
+        io.emit( "jveaV", msg );
+    });
+//--------------------cues para secciones en teclas  
+    
+    socket.on( "mandaImprimir", function( msg ){
+      io.emit( "imprime", msg );
+    }); 
     
     socket.on( "mandaRefresh", function( msg ){
       io.emit( "recibeRefresh", msg );
-  });
+    });
 
 });//termina io.on
 
